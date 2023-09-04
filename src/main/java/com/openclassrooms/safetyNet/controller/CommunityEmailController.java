4b721ce0d92034f3cfe7cc44377c713e0fad0384
J-sicard <ptiptilu@hotmail.fr>
2023-09-04 15:08:08 +0200

    Modification controller for return Json Object

--- src/main/java/com/openclassrooms/safetyNet/controller/CommunityEmailController.java
+++ src/main/java/com/openclassrooms/safetyNet/controller/CommunityEmailController.java
@@ -3,6 +3,7 @@ package com.openclassrooms.safetyNet.controller;
 import com.openclassrooms.safetyNet.view.LinkUrl;
 import com.openclassrooms.safetyNet.dao.communityEmailDao.CommunityEmailDAO;
 import com.openclassrooms.safetyNet.model.CommunityEmail;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,13 @@ public class CommunityEmailController {
     }
 
     @GetMapping(LinkUrl.URL_FOR_EMAIL_OF_AL_CITY_RESIDENTS)
-    public List<CommunityEmail> emailAddressesOfEveryoneInTheCity(@RequestParam(required = true) String city){
-        return communityEmailDAO.findByCity(city);
+    public String emailAddressesOfEveryoneInTheCity(@RequestParam(required = true) String city){
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(communityEmailDAO.findByCity(city));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
--- src/main/java/com/openclassrooms/safetyNet/controller/PhoneAlertController.java
+++ src/main/java/com/openclassrooms/safetyNet/controller/PhoneAlertController.java
@@ -1,9 +1,10 @@
 package com.openclassrooms.safetyNet.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.openclassrooms.safetyNet.dao.phoneAlertDao.PhoneAlertDAO;
-import com.openclassrooms.safetyNet.model.FireSationInformation;
 import com.openclassrooms.safetyNet.model.PhoneAlert;
 import com.openclassrooms.safetyNet.view.LinkUrl;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +21,15 @@ public class PhoneAlertController{
     }
 
     @GetMapping(LinkUrl.URL_FOR_PHONE_NUMBERS_OF_RESIDENTS_SERVED_BY_THE_FIRE_STATION)
-    public List<PhoneAlert> addressOfSation(@RequestParam(required = true) String station_number){
-        return phoneAlertDAO.findByFireSationNumber(station_number);
+    public String addressOfSation(@RequestParam(required = true) String station_number) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            List<PhoneAlert> phoneAlerts = phoneAlertDAO.findByFireSationNumber(station_number);
+            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(phoneAlerts);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
 
