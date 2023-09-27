bd08499e71e4cfcff3c5d7153d7171ffff6335c4
J-sicard <ptiptilu@hotmail.fr>
2023-09-27 15:58:57 +0200

    createPersonTest create

--- src/test/java/com/openclassrooms/safetyNet/testcontroller/PersonControllerTest.java
+++ src/test/java/com/openclassrooms/safetyNet/testcontroller/PersonControllerTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -17,7 +19,16 @@ public class PersonControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    public void testDeleteMedicalRecord() throws Exception {
+    public void testPostMedicalRecord() throws Exception {
+        mockMvc.perform(post("/person")
+                .contentType(MediaType.APPLICATION_JSON) // Utilisez MediaType.APPLICATION_JSON
+                .content("{ \"firstName\" : \"Rayane\", \"lastName\" : \"Berrada\", \"birthdate\" : \"26/12/1994\", \"medications\" : [\"Morphine\"], \"allergies\" : [ \"shellfish\" ] }")
+        ).andExpect(status().is2xxSuccessful());
+    }
+
+
+    @Test
+    public void deletePersonTest() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders
                 .delete("/person")
                 .contentType(MediaType.APPLICATION_JSON)
