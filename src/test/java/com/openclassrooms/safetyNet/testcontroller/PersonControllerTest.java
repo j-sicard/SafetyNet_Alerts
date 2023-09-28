787a251a2bf50c5438336dd2ca6f030212c19f93
J-sicard <ptiptilu@hotmail.fr>
2023-09-28 17:37:45 +0200

    modify createPersonTest, mock data-base

--- src/test/java/com/openclassrooms/safetyNet/testcontroller/PersonControllerTest.java
+++ src/test/java/com/openclassrooms/safetyNet/testcontroller/PersonControllerTest.java
@@ -1,14 +1,24 @@
 package com.openclassrooms.safetyNet.testcontroller;
 
+import com.openclassrooms.safetyNet.model.FireStation;
+import com.openclassrooms.safetyNet.model.Person;
+import com.openclassrooms.safetyNet.service.PersonService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
 
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,12 +29,20 @@ public class PersonControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private PersonService personService;
+
     @Test
     public void createPersonTest() throws Exception {
+        List<Person> expectedPerson = new ArrayList<>();
+        when(personService.savePerson(any(Person.class))).thenReturn(expectedPerson);
+
         mockMvc.perform(post("/person")
                 .contentType(MediaType.APPLICATION_JSON) // Utilisez MediaType.APPLICATION_JSON
                 .content("{ \"firstName\" : \"fabien\", \"lastName\" : \"Bartez\", \"address\" : \" 1 address test\", \"city\" : \"Culver\", \"zip\" :  \"97451\", \"phone\" : \"841-874-6512\", \"email\" : \"jaboyd@email.com\"  }")
         ).andExpect(status().is2xxSuccessful());
+
+        verify(personService).savePerson(any(Person.class));
     }
 
 
