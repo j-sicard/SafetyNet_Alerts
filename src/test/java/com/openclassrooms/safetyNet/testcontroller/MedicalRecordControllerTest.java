347576b25cb50c4a5917793c4d727668688ad560
J-sicard <ptiptilu@hotmail.fr>
2023-09-28 17:28:43 +0200

    modify createMedicalRecordTest, mock data-base

--- src/test/java/com/openclassrooms/safetyNet/testcontroller/MedicalRecordControllerTest.java
+++ src/test/java/com/openclassrooms/safetyNet/testcontroller/MedicalRecordControllerTest.java
@@ -1,13 +1,23 @@
 package com.openclassrooms.safetyNet.testcontroller;
 
+import com.openclassrooms.safetyNet.controller.MedicalRecordController;
+import com.openclassrooms.safetyNet.model.MedicalRecord;
+import com.openclassrooms.safetyNet.service.MedicalRecordService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,12 +29,31 @@ public class MedicalRecordControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
+  /*  @Test
     public void createMedicalRecordTest() throws Exception {
+
+
         mockMvc.perform(post("/medicalRecord")
                 .contentType(MediaType.APPLICATION_JSON) // Utilisez MediaType.APPLICATION_JSON
                 .content("{ \"firstName\" : \"fabien\", \"lastName\" : \"Bartez\", \"birthdate\" : \"10/11/1968\", \"medications\" : [\"Doliprane\"], \"allergies\" : [ \"cacahuete\" ] }")
         ).andExpect(status().is2xxSuccessful());
+    }*/
+
+
+    @MockBean
+    private MedicalRecordService medicalRecordService;
+    @Test
+    public void createMedicalRecordTest() throws Exception {
+        List<MedicalRecord> medicalRecords = new ArrayList<>();
+
+        when(medicalRecordService.saveMedicalRecord(any(MedicalRecord.class))).thenReturn(medicalRecords);
+
+        mockMvc.perform(post("/medicalRecord")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"firstName\" : \"fabien\", \"lastName\" : \"Bartez\", \"birthdate\" : \"10/11/1968\", \"medications\" : [\"Doliprane\"], \"allergies\" : [ \"cacahuete\" ] }")
+        ).andExpect(status().is2xxSuccessful());
+
+        verify(medicalRecordService).saveMedicalRecord(any(MedicalRecord.class));
     }
 
 
