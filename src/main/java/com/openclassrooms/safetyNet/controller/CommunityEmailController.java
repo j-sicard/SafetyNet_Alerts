c76565fbf6c349cb0f232902270d466e07e64974
J-sicard <ptiptilu@hotmail.fr>
2023-09-29 15:05:55 +0200

    CommunityEmailController GetMapping modify, try/catch LOGGER create

--- src/main/java/com/openclassrooms/safetyNet/controller/CommunityEmailController.java
+++ src/main/java/com/openclassrooms/safetyNet/controller/CommunityEmailController.java
@@ -3,6 +3,8 @@ package com.openclassrooms.safetyNet.controller;
 
 import com.openclassrooms.safetyNet.service.PersonService;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,8 +18,16 @@ public class CommunityEmailController {
     @Autowired
     PersonService personService;
 
+    private static final Logger LOGGER =  LogManager.getLogger( ChildrenAlertController.class );
+
     @GetMapping(value = "/communityEmail")
-    public List<String> getCommunityEmails(@RequestParam String city) throws ClassNotFoundException, IOException {
-        return personService.getPersonEmailFromCity(city);
+    public List<String> getCommunityEmails(@RequestParam String city){
+        try {
+            LOGGER.info(personService.getPersonEmailFromCity(city));
+            return personService.getPersonEmailFromCity(city);
+        }catch (ClassNotFoundException | IOException e){
+            LOGGER.error("Impossible de lire le fichier data.json");
+            return null;
+        }
     }
 }
